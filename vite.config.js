c89cc8c7d947396ca925970d4ad6ac66925c5e50
carolwang888 <1306969202@qq.com>
2026-04-14 15:16:32 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ielts-dictation/',
+base: '/ielts-dictation/ielts-dictation4.14/',
+
   plugins: [react()],
   server: {
     port: 5173,
